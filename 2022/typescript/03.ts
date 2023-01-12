32988c5a1d15dd6d26e46e8fd84a3fd091b39b4a
Ralin Chimev <ralin.chimev@gmail.com>
2023-01-12 09:04:49 +0200

    Advent of Code 2022 day 3 TypeScript

---
+++ 2022/typescript/03.ts
@@ -0,0 +1,17 @@
+import {readFileSync} from 'fs'
+import _ from 'underscore'
+
+const input = readFileSync('./2022/03.input', 'utf8').trim().split(/\n/)
+
+const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const priority: Record<string, number> = [...alphabet].reduce((a, e, i) => ({...a, [e]: i + 1}), {})
+
+const first = input
+  .map(sack => _.intersection([...sack.slice(0, sack.length / 2)], [...sack.slice(sack.length / 2)])[0])
+  .reduce((prio, item) => prio + priority[item], 0)
+console.log(first) // 7763
+
+const second = _.chunk(input, 3)
+  .map(([sack1, sack2, sack3]) => _.intersection([...sack1], [...sack2], [...sack3])[0])
+  .reduce((prio, item) => prio + priority[item], 0)
+console.log(second) // 2569
